b27d76c3405337c7f46ca5a31256bb449f6d6c5d
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2023-11-15 16:43:53 +0100

    correction calcul len_jobs

--- src/main.c
+++ src/main.c
@@ -7,14 +7,13 @@ char *prompt(int jobs, char *pwd) {
 	char * prompt=malloc(100);
 	
 	int diff=0;
-	int len_jobs=floor(log10((double)jobs))+1;
-	int count=len_jobs+strlen(pwd)+4; /*2 crochets, 1 dollar, 1 espace*/
+	int len_jobs=1; // minimum 0 jobs -> 0 est le longueur 1
+	if(jobs>0) len_jobs=floor(log10((double)jobs))+1;
+	int count=len_jobs+strlen(pwd)+4; //2 crochets, 1 dollar, 1 espace
 
 	if(count>30) {
 		diff=count-30+3;
 		if(strlen(pwd)>diff) sprintf(prompt,"\001\033[33m\002[%d]\001\033[36m\002...%s\001\033[00m\002$ ",jobs,pwd+diff);
-		else printf("oups petit souçi ici...,prompt pas affichable");
-		/*meme si on tronque le chemin c est trop long (la faute au nb de jobs) -> peu probable*/
 		
 	}else{
 		sprintf(prompt,"\001\033[33m\002[%d]\001\033[36m\002%s\001\033[00m\002$ ",jobs,pwd);
@@ -29,11 +28,17 @@ char *prompt(int jobs, char *pwd) {
 }
 
 int main(){
+	char *verif=prompt(0,"------------------------>");
+	printf("%s\n",verif);
 	char *p=prompt(3,"/home/titi/pas/trop/long/mais/la/ca/depasse");
 	printf("%s\n",p);
 	char *p2=prompt(17,"/home/titi");
 	printf("%s\n",p2);
+	char *p3=prompt(0,"/home/donc/ca/commence/ici/");
+	printf("%s\n",p3);
+	free(verif);
 	free(p);
 	free(p2);
+	free(p3);
 	exit(0);
 }
