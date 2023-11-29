1daf929eb7b1803159e520b493fa49474186b6ac
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2023-11-29 10:53:21 +0100

    facto prompt

--- src/prompt.h
+++ src/prompt.h
@@ -1,6 +1,13 @@
+#ifndef PROMPT_H
+#define PROMPT_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <unistd.h>
+
 extern char * prompt;
 extern int update_prompt(int);
+
+#endif
