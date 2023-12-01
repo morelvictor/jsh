2319816a80ad887a70e2cfb8188e4602fd6f0665
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2023-12-01 22:57:06 +0100

    suite redirections et inclusion du .h

--- src/redirections.c
+++ src/redirections.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "redirections.h"
 
 enum redir {a,b,c,d,e,f,g,h,i};
 
-int redirect(int r, char * fic){
+int redirect(int r, char * path){
 	int fd;
 	switch(r){
+		case a : 
+			fd=open(path,O_RDONLY,0600);
+			if(fd!=-1) dup2(fd,STDIN_FILENO);
+			return fd;
 		case b : 
-			fd=open(fic,O_WRONLY | O_CREAT | O_EXCL,0600);
+			fd=open(path,O_WRONLY | O_CREAT | O_EXCL,0600);
 			if(fd!=-1)dup2(fd,STDOUT_FILENO);
 			return(fd);
 		case c :
-			fd=open(fic, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+			fd=open(path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 			if(fd!=-1) dup2(fd,STDOUT_FILENO);
 			return(fd);
 		case d : 
-			fd=open(fic, O_WRONLY | O_CREAT | O_APPEND, 0600);
+			fd=open(path, O_WRONLY | O_CREAT | O_APPEND, 0600);
 			if(fd!=1) dup2(fd,STDOUT_FILENO);
 			return fd;
 		default : return -1;
@@ -26,7 +27,7 @@ int redirect(int r, char * fic){
 }
 
 int main(void){
-	redirect(d,"fic");
+	redirect(b,"fic");
 	execlp("ls","ls",NULL);
 	/*int fd =open("fic",O_WRONLY | O_CREAT,0600);
 	dup2(fd,STDOUT_FILENO);
---
+++ src/redirections.h
@@ -0,0 +1,10 @@
+#ifndef REDIRECTIONS_H
+#define REDIRECTIONS_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int redirect(int,char *);
+
+#endif
