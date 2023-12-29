fcddd3f6b55b5092fb72c869a59227562e43fd8b
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2023-12-29 15:23:11 +0100

    ajout d un mot a un index

--- src/substitutions.c
+++ src/substitutions.c
@@ -46,7 +46,17 @@ void get_cmds_sub(w_index **cmds, w_index *pi, int *t, int *p, int n){
 }
 
 /*int main(){
-	w_index *pi=split_space("cmd1 <( cmd2 ) <( cmd3 )");
+	w_index *pi=split_space("cmd1 <( cmd2 | cmd2.1 | cmd2.2 ) <( cmd3 | cmd3.1 )");
+	
+	pi->words=realloc(pi->words,(pi->size+1)*sizeof(char*));
+	char *nouv="tutu";
+	pi->words[pi->size]=malloc((strlen(nouv)+1)*sizeof(char));
+	strcpy(pi->words[pi->size],nouv);
+	pi->size=pi->size+1;
+	print_index(pi);
+	free_index(pi);
+
+	
 	const int n=count_substitutions(pi);
 	if(n==0) printf("pas de substitution\n");
 	if (n==-1) printf("syntaxe invalide\n");
@@ -55,8 +65,24 @@ void get_cmds_sub(w_index **cmds, w_index *pi, int *t, int *p, int n){
 		int p[n];
 		pos_open_sub(pi,t);
 		pos_close_sub(pi,p);
-		print_tab(t,n);
-		print_tab(p,n);
+		//print_tab(t,n);
+		//print_tab(p,n);
+		w_index **cmds=malloc(n*(sizeof(w_index *)));
+		get_cmds_sub(cmds,pi,t,p,n);
+		for(int i=0; i<n; ++i){
+			print_index(cmds[i]);
+		}
+		
+		char *s=malloc(5*sizeof(char));
+		s[0]='j';
+		s[1]='a';
+		s[2]='n';
+		s[3]='a';
+		char *cp=malloc(5*sizeof(char));
+		strcpy(cp,s);
+		s[0]='n';
+		free(s);
+		printf("%s\n",cp);	
 
 	}
 	exit(0);
