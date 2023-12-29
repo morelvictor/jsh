96c239adca4c6d26a19ee199e93708cdeeaac923
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2023-12-29 01:18:02 +0100

    parsage jet 1

--- src/substitutions.c
+++ src/substitutions.c
@@ -1,4 +1,7 @@
 #include "substitutions.h"
+#include "parser.h"
+
+
 
 int count_substitutions(w_index* pi){
 	int n=0;
@@ -10,3 +13,45 @@ int count_substitutions(w_index* pi){
 	if(n!=m) return -1;
 	else return n;
 }
+
+void pos_open_sub(w_index *pi, int *t){
+	int acc_t=0;	
+	for(int i=0; i<pi->size; ++i){
+		if(strcmp("<(",pi->words[i])==0){
+			t[acc_t]=i;
+			++acc_t;
+		}
+	}
+}
+void pos_close_sub(w_index *pi,int *t){
+	int acc_t=0;	
+	for(int i=0; i<pi->size; ++i){
+		if(strcmp(")",pi->words[i])==0){
+			t[acc_t]=i;
+			++acc_t;
+		}
+	}
+}
+void print_tab(int *t,int len){
+	for(int i=0; i<len; ++i){
+		printf("%d  ",*(t+i));
+	}
+	puts("");
+}
+
+/*int main(){
+	w_index *pi=split_space("cmd1 <( cmd2 ) <( cmd3 )");
+	const int n=count_substitutions(pi);
+	if(n==0) printf("pas de substitution\n");
+	if (n==-1) printf("syntaxe invalide\n");
+	if(n>0){
+		int t[n];
+		int p[n];
+		pos_open_sub(pi,t);
+		pos_close_sub(pi,p);
+		print_tab(t,n);
+		print_tab(p,n);
+
+	}
+	exit(0);
+}*/
--- src/substitutions.h
+++ src/substitutions.h
@@ -8,5 +8,8 @@
 #include "parser.h"
 
 extern int count_substitutions(w_index *);
+extern void pos_open_sub(w_index *,int *);
+extern void pos_close_sub(w_index *,int *);
+extern void print_tab(int *,int);
 
 #endif
