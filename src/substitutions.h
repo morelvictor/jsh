be0242cbee0555bf1205ebfc301bd6c20069cd31
Jana Ayadi <jana.ayadi@etu.u-paris.fr>
2024-01-06 01:02:36 +0100

    ajout substitutions au main

--- src/substitutions.h
+++ src/substitutions.h
@@ -8,14 +8,14 @@
 #include <sys/wait.h>
 #include "parser.h"
 
-extern int count_substitutions(w_index *);
-extern void pos_open_sub(w_index *,int *);
-extern void pos_close_sub(w_index *,int *);
-extern void print_tab(int *,int);
-extern void get_cmds_sub(w_index **, w_index *, int *, int *, int );
-extern int launch_cmd(w_index *);
-extern int launch_test(w_index *);
-extern int is_substituted(w_index *);
-extern w_index * substitute(w_index *);
+int count_substitutions(w_index *);
+void pos_open_sub(w_index *,int *);
+void pos_close_sub(w_index *,int *);
+void print_tab(int *,int);
+void get_cmds_sub(w_index **, w_index *, int *, int *, int );
+int launch_cmd(w_index *);
+int launch_test(w_index *);
+int is_substituted(w_index *);
+w_index * substitute(w_index *);
 
 #endif
